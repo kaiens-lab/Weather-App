2d2dab282c4c1ca82c5f2d720bcd038e8b08b4ca
KN <ckn.kaien@gmail.com>
2025-03-02 18:59:29 +0800

    refactor: update CityInfo to use TypeScript types

--- src/components/CityInfo/CityInfo.tsx
+++ src/components/CityInfo/CityInfo.tsx
@@ -2,12 +2,16 @@ import React, { useEffect, useState } from "react";
 import "./CityInfo.css";
 import Icon from "./Icon";
 
-/*------------------------CityInfo Component------------------------*/
-// Function: Displays the city's weather based on the data received
-// from App.js through props.
+type CityInfoProps = {
+  localtime: string;
+  cityName: string;
+  conditionText: string;
+  conditionIcon: string;
+  isDay: boolean;
+  day: string;
+};
 
-//The data for these parameters comes from App.js.
-const CityInfo = ({
+const CityInfo: React.FC<CityInfoProps> = ({
   localtime,
   cityName,
   conditionText,
@@ -19,13 +23,13 @@ const CityInfo = ({
 
   useEffect(() => {
     if (localtime) {
-      // The API returns `localtime` in this format: "2024-0-01 12:00"
-      const y = parseInt(localtime.substr(0, 4));
-      const d = parseInt(localtime.substr(5, 2));
-      const m = parseInt(localtime.substr(8, 2));
-      const timeStr = localtime.substr(11);
+      // The API returns `localtime` in this format: "2025-03-02 12:00"
+      const y = parseInt(localtime.substring(0, 4));
+      const m = parseInt(localtime.substring(5, 7));
+      const d = parseInt(localtime.substring(8, 10));
+      const timeStr = localtime.substring(11);
 
-      const dayOfTheWeek = (day, month, year) => {
+      const dayOfTheWeek = (day: number, month: number, year: number) => {
         const weekday = [
           "Sunday",
           "Monday",
@@ -35,7 +39,7 @@ const CityInfo = ({
           "Friday",
           "Saturday",
         ];
-        return weekday[new Date(`${day}/${month}/${year}`).getDay()];
+        return weekday[new Date(year, month - 1, day).getDay()];
       };
 
       setDate(`${dayOfTheWeek(d, m, y)} ${d}, ${m} ${y}`);
@@ -53,7 +57,11 @@ const CityInfo = ({
         </div>
       </div>
       <div className="weather-condition">
-        <Icon conditionIcon={conditionIcon} isDay={isDay} />
+        <Icon
+          conditionIcon={conditionIcon}
+          data-testid="icon-component"
+          isDay={isDay}
+        />
         <p className="condition-text">{conditionText}</p>
       </div>
     </div>
