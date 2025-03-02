082610d1306ad926ba2d44ba8e17111dd1471e63
KN <ckn.kaien@gmail.com>
2025-03-02 19:50:23 +0800

    test: add unit tests for Temp

---
+++ src/components/Temp/Temp.test.tsx
@@ -0,0 +1,9 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import Temperature from "./Temp";
+
+test("renders temperature with correct value", () => {
+  render(<Temperature temp="25" />);
+  //   screen.debug();
+  expect(screen.getByText("25°")).toBeInTheDocument();
+});
